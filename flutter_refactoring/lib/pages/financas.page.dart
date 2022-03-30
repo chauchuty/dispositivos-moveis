d2cd685f7c9aca328c9ce07736ac1f65cf9905d8
chauchuty <chauchuty@alunos.utfpr.edu.br>
2022-03-30 01:38:36 -0300

    update: iconsAppBar

---
+++ flutter_refactoring/lib/pages/financas.page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class FinancasPage extends StatelessWidget {
+  const FinancasPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('FINANÇAS'),
+    );
+  }
+}
